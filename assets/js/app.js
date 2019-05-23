ec08ac20d72f0355c97a2085b9d7c34b51dd4235
Alexbeis <alexbeis09@gmail.com>
2019-05-23 20:02:50 +0200

    refactor js dashboard object

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../../node_modules/admin-lte/bower_components/font-awesome/css/font-awesome.min.css';
 import '../../node_modules/admin-lte/bower_components/Ionicons/css/ionicons.min.css';
 import '../../node_modules/admin-lte/dist/css/AdminLTE.min.css';
+import '../../node_modules/admin-lte/dist/css/skins/skin-blue.min.css';
 
 const $ = require('jquery');
 global.$ = global.jquery = $;
@@ -13,9 +14,9 @@ import '../../node_modules/admin-lte/bower_components/jquery-ui/jquery-ui.js';
 import '../../node_modules/admin-lte/dist/js/adminlte.js';
 
 // Custom js
-import './gm.js';
+import './gm/gm.js';
 
 
 
-console.log('Hello Webpack Encore!!!!!!! Edit me in assets/js/app.js');
+
 
---
+++ assets/js/gm/gm.js
@@ -0,0 +1,69 @@
+'use strict';
+
+(function(window, $) {
+
+    window.GrupoMateuDashboard = function($wrapper) {
+        console.log('Hello from constructor');
+        this.$wrapper = $wrapper;
+
+        //TODO: Bind events
+
+        // App Init
+        this.init();
+    };
+
+    $.extend(window.GrupoMateuDashboard.prototype, {
+
+        _selectors: {
+            pluggins:{
+
+            },
+
+        },
+
+        init: function() {
+            // Load External pluggins
+            this._loadAndInitExternalPluggins();
+        },
+
+        _loadAndInitExternalPluggins: function() {
+            // Make the dashboard widgets sortable Using jquery UI
+            $('.connectedSortable').sortable({
+                placeholder         : 'sort-highlight',
+                connectWith         : '.connectedSortable',
+                handle              : '.box-header, .nav-tabs',
+                forcePlaceholderSize: true,
+                zIndex              : 999999
+            });
+            $('.connectedSortable .box-header, .connectedSortable .nav-tabs-custom').css('cursor', 'move');
+
+            // jQuery UI sortable for the todo list
+            $('.todo-list').sortable({
+                placeholder         : 'sort-highlight',
+                handle              : '.handle',
+                forcePlaceholderSize: true,
+                zIndex              : 999999
+            });
+
+            /* The todo list plugin */
+            $('.todo-list').todoList({
+                onCheck  : function () {
+                    window.console.log($(this), 'The element has been checked');
+                },
+                onUnCheck: function () {
+                    window.console.log($(this), 'The element has been unchecked');
+                }
+            });
+        }
+    });
+
+    let BoxesWrapper = $('.js-boxes');
+
+    if (BoxesWrapper.length > 0) {
+        let GM = new GrupoMateuDashboard(BoxesWrapper);
+    }
+
+
+    console.log('From webpack?');
+})(window, jQuery);
+
