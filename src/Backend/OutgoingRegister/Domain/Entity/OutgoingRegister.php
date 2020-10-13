e2792d5db51b1fa4e83d5fb20aab9c6a0b41bd93
Alexbeis <alexbeis09@gmail.com>
2020-10-13 22:28:11 +0200

    OutgoingRegisters WIP

--- src/Backend/OutgoingRegister/Domain/Entity/OutgoingRegister.php
+++ src/Backend/OutgoingRegister/Domain/Entity/OutgoingRegister.php
@@ -2,12 +2,14 @@
 
 namespace Mateu\Backend\OutgoingRegister\Domain\Entity;
 
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\JoinTable;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Doctrine\ORM\Mapping as ORM;
-
+use Mateu\Backend\Animal\Domain\Entity\Animal;
+use Mateu\Backend\User\Domain\Entity\User;
 
 /**
  * @ORM\Entity(repositoryClass="Mateu\Backend\OutgoingRegister\Infraestructure\OutgoingRegisterRepository")
@@ -27,16 +29,33 @@ class OutgoingRegister
      * @ORM\Column(type="string", length=40, nullable=false)
      */
     private $uuid;
+
     /**
      * @ORM\Column(type="text", length=50, nullable=true)
      */
     private $destination;
 
     /**
+     * @ORM\Column(type="text", length=50, nullable=true)
+     */
+    private $outGuide;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $meanWeight;
+
+    /**
      * @ORM\ManyToOne(targetEntity="Mateu\Backend\Purchaser\Domain\Entity\Purchaser", inversedBy="outgoingRegisters")
      * @JoinColumn(name="purchaser_id", referencedColumnName="id")
      */
     private $purchaser;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Mateu\Backend\Transporter\Domain\Entity\Transporter", inversedBy="outgoingRegisters")
+     * @JoinColumn(name="transporter_id", referencedColumnName="id")
+     */
+    private $transporter;
 
     /**
      * @ORM\ManyToMany(
@@ -64,10 +83,238 @@ class OutgoingRegister
      */
     private $animalsCount;
 
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $outDate;
+
     public function __construct()
     {
         $this->animals = new ArrayCollection();
     }
 
+    /**
+     * @param string $uuid
+     *
+     * @return OutgoingRegister
+     */
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+        return $this;
+    }
+
+    /**
+     * @param mixed $destination
+     *
+     * @return OutgoingRegister
+     */
+    public function setDestination($destination)
+    {
+        $this->destination = $destination;
+        return $this;
+    }
+
+    public function addAnimal(Animal $animal)
+    {
+        if (!$this->animals->contains($animal)) {
+            $this->animals->add($animal);
+        }
+    }
+
+    /**
+     * @param mixed $purchaser
+     *
+     * @return OutgoingRegister
+     */
+    public function setPurchaser($purchaser)
+    {
+        $this->purchaser = $purchaser;
+        return $this;
+    }
+
+    /**
+     * @param ArrayCollection $animals
+     *
+     * @return OutgoingRegister
+     */
+    public function setAnimals(ArrayCollection $animals)
+    {
+        $this->animals = $animals;
+        return $this;
+    }
+
+    /**
+     * @param mixed $outType
+     *
+     * @return OutgoingRegister
+     */
+    public function setOutType($outType)
+    {
+        $this->outType = $outType;
+        return $this;
+    }
+
+    /**
+     * @param mixed $created_by
+     *
+     * @return OutgoingRegister
+     */
+    public function setCreatedBy($created_by)
+    {
+        $this->created_by = $created_by;
+        return $this;
+    }
+
+    /**
+     * @param mixed $animalsCount
+     *
+     * @return OutgoingRegister
+     */
+    public function setAnimalsCount($animalsCount)
+    {
+        $this->animalsCount = $animalsCount;
+        return $this;
+    }
+
+    /**
+     * @param DateTime $outDate
+     *
+     * @return OutgoingRegister
+     */
+    public function setOutDate($outDate)
+    {
+        $this->outDate = $outDate;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDestination()
+    {
+        return $this->destination;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPurchaser()
+    {
+        return $this->purchaser;
+    }
+
+    /**
+     * @return null|ArrayCollection
+     */
+    public function getAnimals(): ?ArrayCollection
+    {
+        return $this->animals;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOutType()
+    {
+        return $this->outType;
+    }
+
+    /**
+     * @return User
+     */
+    public function getCreatedBy(): User
+    {
+        return $this->created_by;
+    }
+
+    /**
+     * @return null|integer
+     */
+    public function getAnimalsCount():? int
+    {
+        return $this->animalsCount;
+    }
+
+    /**
+     * @return null | DateTime
+     */
+    public function getOutDate():DateTime
+    {
+        return $this->outDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOutGuide()
+    {
+        return $this->outGuide;
+    }
+
+    /**
+     * @param mixed $outGuide
+     *
+     * @return OutgoingRegister
+     */
+    public function setOutGuide($outGuide)
+    {
+        $this->outGuide = $outGuide;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMeanWeight()
+    {
+        return $this->meanWeight;
+    }
+
+    /**
+     * @param mixed $meanWeight
+     *
+     * @return OutgoingRegister
+     */
+    public function setMeanWeight($meanWeight)
+    {
+        $this->meanWeight = $meanWeight;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTransporter()
+    {
+        return $this->transporter;
+    }
+
+    /**
+     * @param mixed $transporter
+     *
+     * @return OutgoingRegister
+     */
+    public function setTransporter($transporter)
+    {
+        $this->transporter = $transporter;
+        return $this;
+    }
 
 }
--- src/Backend/OutgoingRegister/Infraestructure/OutgoingRegisterRepository.php
+++ src/Backend/OutgoingRegister/Infraestructure/OutgoingRegisterRepository.php
@@ -3,7 +3,9 @@
 namespace Mateu\Backend\OutgoingRegister\Infraestructure;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Persistence\ManagerRegistry;
 use Mateu\Backend\OutgoingRegister\Domain\Entity\OutgoingRegister;
 use Mateu\Backend\OutgoingRegister\Domain\OutgoingRegisterRepositoryInterface;
 
@@ -13,5 +15,93 @@ class OutgoingRegisterRepository extends ServiceEntityRepository implements Outg
     {
         parent::__construct($registry, OutgoingRegister::class);
     }
+
+    public function getPaginatedOutgoingRegisters(array $data)
+    {
+        $start = $data['start'] ? $data['start'] : 0;
+        $limit = $data['length'] ? $data['length'] : 10;
+        $search = $data['search']['value'];
+
+        list($orderBy, $order) = $this->getOrderByData($data);
+
+        $qb = $this->createQueryBuilder('out_r');
+
+
+        switch ($orderBy) {
+            case 'type':
+                $qb->orderBy("out_r.outType", $order);
+                break;
+            case 'destination':
+                $qb->orderBy("out_r.destination", $order);
+                break;
+            case 'animalsCount':
+                $qb->orderBy('out_r.animalsCount', $order);
+                break;
+            default:
+                $qb->orderBy("out_r.createdAt", $order);
+        }
+
+        if ($search) {
+            $qb->innerJoin('out_r.outType', 'outType');
+
+            $qb->where(
+                $qb->expr()->orX(
+                    $qb->expr()->like('outType.name','?1'),
+                    $qb->expr()->like('out_r.destination', '?1')
+                )
+            )->setParameter('1', '%'.$search.'%');
+        }
+
+        $query = $qb->getQuery();
+
+        $paginator = $this->paginate($query, $start, $limit);
+
+        $result = [];
+
+        $result['data'] = array_map(function ($element) {
+            /**
+             * @var OutgoingRegister $element
+             */
+            return [
+                'type' =>$element->getOutType()->getName(),
+                'destination' => $element->getDestination(),
+                'animalsCount' => $element->getAnimalsCount(),
+                'createdAt' => $element->getCreatedAt()->format('d-m-y'),
+                'createdBy' => $element->getCreatedBy()->getUsername(),
+                'uuid' => $element->getUuid()
+            ];
+        }, $paginator->getQuery()->execute());
+
+        return array_merge(
+            $result,
+            [
+                'recordsTotal' => $paginator->count(),
+                'recordsFiltered' => $paginator->count()
+            ]
+        );
+
+    }
+
+    private function paginate(Query $query, int $start, int $limit)
+    {
+        $paginator = new Paginator($query);
+
+        $paginator->getQuery()
+            ->setFirstResult($start)
+            ->setMaxResults($limit);
+
+        return $paginator;
+    }
+
+    private function getOrderByData($data)
+    {
+        $order = $data['order'][0]['dir'];
+        $orderByindex = $data['order'][0]['column'];
+
+        $orderBy = $data['columns'][$orderByindex]['data'];
+
+        return [$orderBy, $order];
+    }
+
 
 }
